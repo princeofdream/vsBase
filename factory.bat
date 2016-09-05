e7f40cb19e43044119dbab4f269561f304f2cece
Li jin <lijin@hgrica.com>
2016-09-05 11:54:39 +0800

    update

--- factory.bat
+++ factory.bat
@@ -36,33 +36,33 @@ REM  goto END
 
 :WRITE_ID
 	ECHO "Writing your ID..."
-	adb shell "mkdir -p /mnt/protect_t/ /mnt/protect_s/"
-	adb shell "mount -t ext4 /dev/block/platform/mtk-msdc.0/by-name/protect1 /mnt/protect_t/"
-	adb shell "mount -t ext4 /dev/block/platform/mtk-msdc.0/by-name/protect2 /mnt/protect_s/"
-	adb shell "mkdir -p /mnt/protect_t/IBoxConfig/"
-	adb shell "mkdir -p /mnt/protect_s/IBoxConfig/"
+	adb shell "mkdir -p /protect_t/ /protect_s/"
+	adb shell "mount -t ext4 /dev/block/platform/mtk-msdc.0/by-name/protect1 /protect_t/"
+	adb shell "mount -t ext4 /dev/block/platform/mtk-msdc.0/by-name/protect2 /protect_s/"
+	adb shell "mkdir -p /protect_t/IBoxConfig/"
+	adb shell "mkdir -p /protect_s/IBoxConfig/"
 
-	adb push IBoxConfig\BroadcastConfig.txt /mnt/protect_s/IBoxConfig/
-	adb push IBoxConfig\DeviceIDConfig.txt  /mnt/protect_s/IBoxConfig/
-	adb push IBoxConfig\OBDServerConfig.txt /mnt/protect_s/IBoxConfig/
-	adb push IBoxConfig\SocketConfig.txt /mnt/protect_s/IBoxConfig/
-	adb push IBoxConfig\CaptureConfig.txt /mnt/protect_s/IBoxConfig/
-	adb push IBoxConfig\FolderConfig.txt /mnt/protect_s/IBoxConfig/
-	adb push IBoxConfig\RTSPConfig.txt /mnt/protect_s/IBoxConfig/
-	adb push IBoxConfig\VideoRecordConfig.txt /mnt/protect_s/IBoxConfig/
+	adb push IBoxConfig\BroadcastConfig.txt /protect_s/IBoxConfig/
+	adb push IBoxConfig\DeviceIDConfig.txt  /protect_s/IBoxConfig/
+	adb push IBoxConfig\OBDServerConfig.txt /protect_s/IBoxConfig/
+	adb push IBoxConfig\SocketConfig.txt /protect_s/IBoxConfig/
+	adb push IBoxConfig\CaptureConfig.txt /protect_s/IBoxConfig/
+	adb push IBoxConfig\FolderConfig.txt /protect_s/IBoxConfig/
+	adb push IBoxConfig\RTSPConfig.txt /protect_s/IBoxConfig/
+	adb push IBoxConfig\VideoRecordConfig.txt /protect_s/IBoxConfig/
 
-	adb push IBoxConfig\BroadcastConfig.txt /mnt/protect_t/IBoxConfig/
-	adb push IBoxConfig\DeviceIDConfig.txt  /mnt/protect_t/IBoxConfig/
-	adb push IBoxConfig\OBDServerConfig.txt /mnt/protect_t/IBoxConfig/
-	adb push IBoxConfig\SocketConfig.txt /mnt/protect_t/IBoxConfig/
-	adb push IBoxConfig\CaptureConfig.txt /mnt/protect_t/IBoxConfig/
-	adb push IBoxConfig\FolderConfig.txt /mnt/protect_t/IBoxConfig/
-	adb push IBoxConfig\RTSPConfig.txt /mnt/protect_t/IBoxConfig/
-	adb push IBoxConfig\VideoRecordConfig.txt /mnt/protect_t/IBoxConfig/
+	adb push IBoxConfig\BroadcastConfig.txt /protect_t/IBoxConfig/
+	adb push IBoxConfig\DeviceIDConfig.txt  /protect_t/IBoxConfig/
+	adb push IBoxConfig\OBDServerConfig.txt /protect_t/IBoxConfig/
+	adb push IBoxConfig\SocketConfig.txt /protect_t/IBoxConfig/
+	adb push IBoxConfig\CaptureConfig.txt /protect_t/IBoxConfig/
+	adb push IBoxConfig\FolderConfig.txt /protect_t/IBoxConfig/
+	adb push IBoxConfig\RTSPConfig.txt /protect_t/IBoxConfig/
+	adb push IBoxConfig\VideoRecordConfig.txt /protect_t/IBoxConfig/
 
-	adb shell "echo %DeviceID% > /mnt/protect_s/IBoxDeviceID.config"
-	adb shell "echo %DeviceID% > /mnt/protect_t/IBoxDeviceID.config"
-	adb shell "cat /mnt/protect_s/IBoxDeviceID.config"
+	adb shell "echo %DeviceID% > /protect_s/IBoxDeviceID.config"
+	adb shell "echo %DeviceID% > /protect_t/IBoxDeviceID.config"
+	adb shell "cat /protect_s/IBoxDeviceID.config"
 	adb shell sync
 
 	set /p check_device_id="Check Your Device ID, Does it right?(Y/N) "
