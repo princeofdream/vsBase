8c6fe62f8b142232dbd0dc745a7471f17a9a3189
James <wingodr@outlook.com>
2016-10-01 19:48:38 +0800

    Add Change project name script

---
+++ change_project_name.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+PROJECT_NAME=$1
+
+if [ "$PROJECT_NAME" == "" ];
+then
+	echo " ------ Exit ------ "
+	exit -1;
+fi
+
+function change_file_content()
+{
+	sed -i "s/vsBase/$PROJECT_NAME/g" vsBase.*
+	sed -i "s/vsBase/$PROJECT_NAME/g" .gitignore
+	find -name "*.cpp" |xargs sed -i "s/vsBase/$PROJECT_NAME/g"
+	find -name "*.h" |xargs sed -i "s/vsBase/$PROJECT_NAME/g"
+}
+
+
+function change_file_name()
+{
+	echo "======= Start ======="
+	rm -rf tmp.sh
+	# echo "#!/bin/sh" > tmp.sh
+	find -name "*vsBase*" >> tmp.sh
+	sed "p; s/vsBase/$PROJECT_NAME/" tmp.sh | sed "N; s/\n/ /" > run.sh
+	sed -i 's/^/MV\ &/g' run.sh
+	chmod a+x run.sh
+	echo "======= END ======="
+}
+
+
+### Main ##
+change_file_content
+change_file_name
+
+./run.sh
+
+rm tmp.sh
+
+
